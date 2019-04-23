edab5fa50e044a0909f8c296e4a038e900b14b0c
necro-wbj <d04220837@gm.tut.edu.tw>
2019-04-23 11:11:29 +0800

    修復數個BUG及改善效能

--- Game.py
+++ Game.py
@@ -3,23 +3,21 @@ import random
 import scan
 import Main_TrainingReversi as mtr
 import TrainingReversi as tr
-from flask import Flask
+# from flask import Flask
 
 
 player = -1
-p1 = list()
-p2 = list()
 Record = True
 chessboard = np.zeros((8, 8))
+p1 = np.array([])
+p2 = np.array([])
 
 
 def start():
-    global player, p1, p2, Record, chessboard
+    global player, Record, chessboard
     Record = True
     chessboard = np.zeros((8, 8))
     player = -1
-    p1 = list()
-    p2 = list()
     chessboard[3][4] = -1
     chessboard[3][3] = 1
     chessboard[4][3] = -1
@@ -27,14 +25,6 @@ def start():
     dissplay(chessboard)
     print('..................................................')
     return chessboard, player
-
-
-def end(chessboard):
-    iter = chessboard.flat
-    for i in iter:
-        if i == 0:
-            return True
-    return False
 
 
 def check(player):
@@ -62,7 +52,8 @@ def dissplay(chessboard):
                 result[j] = '○'
             else:
                 result[j] = '●'
-        print(f' {i}| {result[0]} | {result[1]} | {result[2]} | {result[3]} | {result[4]} | {result[5]} | {result[6]} | {result[7]} | ')
+        print(
+            f' {i}| {result[0]} | {result[1]} | {result[2]} | {result[3]} | {result[4]} | {result[5]} | {result[6]} | {result[7]} | ')
         print('{:-<45}'.format(''))
 
 
@@ -71,9 +62,9 @@ row = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
 
 
 def gamming(manual=False, Probability=0):
-    global memory, batch, input_data, expect_data, Record
+    global memory, batch, input_data, expect_data, Record, p1, p2
     chessboard, player = start()
-    while end(chessboard):
+    while np.any(chessboard == 0):
         cnaDown = check(player)
         if np.any(cnaDown.reshape(-1)):
             if player == -1:
@@ -82,8 +73,9 @@ def gamming(manual=False, Probability=0):
                 predict_opt = mtr.predict_opt(chessboard)
             end_game = False
             predict_opt[cnaDown == False] = np.nan
-            max_value = np.nanmax(predict_opt)
+            max_filter = predict_opt == np.nanmax(predict_opt)
             predict_opt = np.nan_to_num(predict_opt)
+            predict_opt[max_filter] = np.inf
             if manual and player == -1:
                 memory = 1
                 while True:
@@ -105,19 +97,33 @@ def gamming(manual=False, Probability=0):
                 if len(predict_opt[cnaDown]) > 1 and random.randint(1, 100) <= Probability:
                     Record = False
                     max_value = np.random.choice(predict_opt[cnaDown])
-                chessboard[predict_opt == max_value] = player
-            px, py = np.where(predict_opt.reshape(8, 8) == max_value)
+                chessboard[np.isinf(predict_opt)] = player
+            px, py = np.nonzero(np.isinf(predict_opt.reshape(8, 8)))
             px = px.astype(int)[0]
             py = py.astype(int)[0]
             chessboard = scan.update(chessboard, px, py, player)
-            dissplay(chessboard)
+            # dissplay(chessboard)
             if Record:
                 if player == -1:
-                    p1.append([chessboard.reshape(-1),
-                               predict_opt.reshape(-1)])
+                    try:
+                        p1 = np.append(
+                            p1, np.stack(
+                                (chessboard.reshape(-1) * -1, predict_opt.reshape(-1))).reshape(1, 2, 64), 0
+                        )
+                    except:
+                        p1 = np.stack(
+                            (chessboard.reshape(-1)*-1, predict_opt.reshape(-1))
+                        ).reshape(1, 2, 64)
                 else:
-                    p2.append([chessboard.reshape(-1),
-                               predict_opt.reshape(-1)])
+                    try:
+                        p2 = np.append(
+                            p2, np.stack(
+                                (chessboard.reshape(-1), predict_opt.reshape(-1))).reshape(1, 2, 64), 0
+                        )
+                    except:
+                        p2 = np.stack(
+                            (chessboard.reshape(-1), predict_opt.reshape(-1))
+                        ).reshape(1, 2, 64)
             print(f'下的位置: {row[py]}{px}    隨機下: {not Record}')
         else:
             if end_game:
@@ -131,12 +137,35 @@ def gamming(manual=False, Probability=0):
 
 error_test = ['D2', 'C2', 'E5', 'F3', 'F4', 'E2', 'C3', 'B3', 'G2', 'C5', 'C4', 'B5', 'F2', 'F6', 'E6', 'G1', 'C1', 'E1', 'F1', 'G0', 'H0', 'H3', 'H2', 'B0', 'H4', 'H1', 'F0', 'E7',
               'D0', 'G3', 'G4', 'D1', 'C0', 'E0', 'A0', 'G5', 'H5', 'H6', 'H7', 'G6', 'G7', 'F7', 'D7', 'D6', 'D5', 'A6', 'A5', 'A4', 'A3', 'A2', 'C6', 'C7', 'B4', 'B6', 'A7', 'B1', 'A1', 'B2', 'B7']
-input_data = list()
-expect_data = list()
 batch = 0
 memory = 10
 while True:
-    if batch >= 60 * memory:
+    if batch < 60 * memory:
+        player_black, player_white = gamming(manual=manual, Probability=0)
+        input_data = np.concatenate((p1[:, 0], p2[:, 0]))
+        black = p1[:, 1]
+        white = p2[:, 1]
+        if player_black == player_white:
+            print(
+                f"平手!!\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
+            black[np.isinf(black)] = 1
+            white[np.isinf(white)] = 1
+        else:
+            if player_black > player_white:
+                print(
+                    f"黑棋獲勝\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
+                black[np.isinf(black)] = 1
+                white[np.isinf(white)] = -1
+            else:
+                print(
+                    f"白棋獲勝\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
+                black[np.isinf(black)] = -1
+                white[np.isinf(white)] = 1
+        expect_data = np.concatenate((black, white))
+        batch = np.size(input_data, 0)
+        print(f'累計棋盤數: {batch}')
+        tr.train(input_data, expect_data, batch)
+    else:
         player_black, player_white = gamming(Probability=0)
         if player_black < player_white:
             print(f"主要網路獲勝 => 重新訓練")
@@ -148,41 +177,5 @@ while True:
             print("平手!! => 重新訓練敵對網路")
             tr.update_weights(input_data, expect_data, batch)
         batch = 0
-        input_data = list()
-        expect_data = list()
-    else:
-        player_black, player_white = gamming(manual=manual, Probability=0)
-        if player_black > player_white:
-            print(
-                f"黑棋獲勝\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
-            for dataset in p1:
-                input_data.append(dataset[0])
-                dataset[1][dataset[1] == np.amax(dataset[1])] = 1
-                expect_data.append(dataset[1])
-            for dataset in p2:
-                input_data.append(dataset[0])
-                dataset[1][dataset[1] == np.amax(dataset[1])] = 0
-                expect_data.append(dataset[1])
-        elif player_black < player_white:
-            print(
-                f"白棋獲勝\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
-            for dataset in p1:
-                dataset[1][dataset[1] == np.amax(dataset[1])] = 0
-                input_data.append(dataset[0])
-                expect_data.append(dataset[1])
-            for dataset in p2:
-                input_data.append(dataset[0])
-                dataset[1][dataset[1] == np.amax(dataset[1])] = 1
-                expect_data.append(dataset[1])
-        else:
-            print(
-                f"平手!!\t黑棋數量: {player_black}\t白旗數量: {player_white}", end='\t')
-            for dataset in p1:
-                input_data.append(dataset[0])
-                expect_data.append(dataset[1])
-            for dataset in p2:
-                input_data.append(dataset[0])
-                expect_data.append(dataset[1])
-        batch = len(input_data)
-        print(f'累計棋盤數: {batch}')
-        tr.train(input_data, expect_data, batch)
+        del p1
+        del p2
--- Main_TrainingReversi.py
+++ Main_TrainingReversi.py
@@ -6,9 +6,10 @@ import os
 
 def create_model():
     print("正在建立類神經網路模型...")
-    model = keras.Sequential()
     activation = keras.activations.softsign
-    model.add(keras.layers.Dense(100, activation=activation, input_shape=(64,)))
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(
+        64, activation=activation, input_shape=(64,)))
     for _ in range(100):
         model.add(keras.layers.Dense(100, activation=activation))
     model.add(keras.layers.Dense(
@@ -16,37 +17,45 @@ def create_model():
     print("正在編譯類神經網路...")
     model.compile(
         optimizer=keras.optimizers.Nadam(),
-                  loss=keras.losses.MAE)
+        loss=keras.losses.MAE)
     return model
 
 
 if os.path.isfile('./Main_Reversi.h5'):
     print('初始化: ')
-    model = keras.models.load_model('Main_Reversi.h5')
+    Mmodel = keras.models.load_model('Main_Reversi.h5')
     print("讀取已保存的主要網路模型...")
 else:
-    main_checkpoint_path = './Main_Reversi.ckpt'
-    checkpoint_path = './Reversi.ckpt'
-    checkpoint_dir = os.path.dirname(main_checkpoint_path)
-    model = create_model()
+    Mmodel = create_model()
     print('讀取錯誤!!重新建立主要網路模型...')
 
 
 def predict_opt(chessboard):
-    chessboard = np.array(chessboard).reshape(1, 64)
-    output = model.predict(chessboard).reshape(8, 8)
-    return output
+    chessboard = chessboard.reshape(1, 64)
+    output = Mmodel.predict(chessboard)
+    return output.reshape(8, 8)
+
 
 def update_weights(input_data, expect_data, batch):
+    global Mmodel
     import TrainingReversi as tr
-    global model
-    tr.reload()
-    print("主要網路更新權重...")
-    model = keras.models.load_model('Reversi.h5')
-    model.save('Main_Reversi.h5')
+    Mmodel.set_weights(tr.weights())
+    print("儲存...")
+    Mmodel.save('Main_Reversi.h5')
+    print("儲存完成!!")
 
-def reload():
-    global model
-    print('釋放GPU資源...')
-    keras.backend.clear_session()
-    model = keras.models.load_model('Reversi.h5')
+
+# def reload():
+#     global model
+#     try:
+#         if os.path.isfile('./Main_Reversi.h5'):
+#             print('重新讀取權重...')
+#             model = keras.models.load_model('Reversi.h5')
+#         else:
+#             print('尚未有權重，執行儲存...')
+#             model.save('Main_Reversi.h5')
+#     except:
+#         print('釋放GPU資源...')
+#         keras.backend.clear_session()
+#         model = keras.models.load_model('Reversi.h5')
+
