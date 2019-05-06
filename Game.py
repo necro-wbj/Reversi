ac5fd0c12d7e0a6ebe46171ec15cd3ca76b6a029
necro-wbj <d04220837@gm.tut.edu.tw>
2019-05-06 13:23:02 +0800

    修復陣列索引BUG，邏輯BUG，避免使用多個類神經網路導致混用的問題。

--- Game.py
+++ Game.py
@@ -1,9 +1,7 @@
 import numpy as np
 import random
 import scan
-import Main_TrainingReversi as mtr
 import TrainingReversi as tr
-# from flask import Flask
 
 
 player = -1
@@ -18,12 +16,12 @@ def start():
     Record = True
     chessboard = np.zeros((8, 8))
     player = -1
-    chessboard[3][4] = -1
-    chessboard[3][3] = 1
-    chessboard[4][3] = -1
-    chessboard[4][4] = 1
+    chessboard[3,4] = -1
+    chessboard[3,3] = 1
+    chessboard[4,3] = -1
+    chessboard[4,4] = 1
     # dissplay(chessboard)
-    # print('..................................................')
+    print('>'*90)
     return chessboard, player
 
 
@@ -70,21 +68,22 @@ manual = False  # 是否手動遊玩
 row = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
 
 
-def gamming(manual=False, Probability=0):
+def gamming(manual=False, Probability=0 , test = False):
     global memory, batch, input_data, expect_data, Record, p1, p2
     chessboard, player = start()
     while np.any(chessboard == 0):
         cnaDown = check(chessboard,player)
         if np.any(cnaDown.reshape(-1)):
             if player == -1:
-                predict_opt = tr.predict_opt(chessboard * -1)
+                predict_opt = tr.predict_opt(chessboard * -1,Main=False)
+            elif test:
+                predict_opt = tr.predict_opt(chessboard,Main=True)
             else:
-                predict_opt = mtr.predict_opt(chessboard)
+                predict_opt = tr.predict_opt(chessboard, Main=False)
             end_game = False
             predict_opt[cnaDown == False] = np.nan
             max_filter = predict_opt == np.nanmax(predict_opt)
-            predict_opt = np.nan_to_num(predict_opt)
-            predict_opt[max_filter] = np.inf
+            predict_opt[np.isnan(predict_opt)] = -1
             if manual and player == -1:
                 memory = 1
                 while True:
@@ -92,10 +91,9 @@ def gamming(manual=False, Probability=0):
                         playX, playY = input("請輸入位置：")
                         playX = ord(playX.upper())-65
                         playY = int(playY)
-                        if predict_opt[playY][playX] != 0:
-                            chessboard[playY][playX] = -1
-                            predict_opt[playY][playX] = 1
-                            max_value = 1
+                        if predict_opt[playY,playX] != 0:
+                            chessboard[playY,playX] = -1
+                            predict_opt[playY,playX] = np.inf
                             break
                         else:
                             print('錯誤！違反遊戲規則')
@@ -105,8 +103,11 @@ def gamming(manual=False, Probability=0):
                 Record = True
                 if len(predict_opt[cnaDown]) > 1 and random.randint(1, 100) <= Probability:
                     Record = False
-                    max_value = np.random.choice(predict_opt[cnaDown])
-                chessboard[max_filter] = player
+                    max_filter = predict_opt == np.random.choice(
+                        predict_opt[cnaDown])
+                else:
+                    chessboard[max_filter] = player
+                    predict_opt[max_filter] = np.inf
             px, py = np.nonzero(max_filter)
             px = px.astype(int)[0]
             py = py.astype(int)[0]
@@ -147,10 +148,10 @@ def gamming(manual=False, Probability=0):
 error_test = ['D2', 'C2', 'E5', 'F3', 'F4', 'E2', 'C3', 'B3', 'G2', 'C5', 'C4', 'B5', 'F2', 'F6', 'E6', 'G1', 'C1', 'E1', 'F1', 'G0', 'H0', 'H3', 'H2', 'B0', 'H4', 'H1', 'F0', 'E7',
               'D0', 'G3', 'G4', 'D1', 'C0', 'E0', 'A0', 'G5', 'H5', 'H6', 'H7', 'G6', 'G7', 'F7', 'D7', 'D6', 'D5', 'A6', 'A5', 'A4', 'A3', 'A2', 'C6', 'C7', 'B4', 'B6', 'A7', 'B1', 'A1', 'B2', 'B7']
 batch = 0
-memory = 10
+memory = 50
 while True:
     if batch < 60 * memory:
-        player_black, player_white = gamming(manual=manual, Probability=0)
+        player_black, player_white = gamming(manual=manual, Probability=0,test=True)
         input_data = np.concatenate((p1[:, 0], p2[:, 0]))
         black = p1[:, 1]
         white = p2[:, 1]
@@ -178,13 +179,12 @@ while True:
         player_black, player_white = gamming(Probability=0)
         if player_black < player_white:
             print(f"主要網路獲勝 => 重新訓練")
-            tr.update_weights(input_data, expect_data, batch)
+            tr.update_weights(input_data, expect_data, batch,Loss=True)
         elif player_black > player_white:
             print("主要網路敗北 => 更新強度為敵對網路")
-            mtr.update_weights(input_data, expect_data, batch)
+            tr.update_weights(input_data, expect_data, batch,Loss=False)
         else:
             print("平手!! => 重新訓練敵對網路")
             tr.update_weights(input_data, expect_data, batch)
         batch = 0
-        del p1
-        del p2
+        del p1,p2,input_data,expect_data
--- TrainingReversi.py
+++ TrainingReversi.py
@@ -1,47 +1,84 @@
-import Main_TrainingReversi as mt
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import os
 
+
+def create_model():
+    print("正在建立類神經網路模型...")
+    activation = keras.activations.softsign
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(
+        64, activation=activation, input_shape=(64,)))
+    for _ in range(100):
+        model.add(keras.layers.Dense(100, activation=activation))
+    model.add(keras.layers.Dense(
+        64, activation=activation))
+    print("正在編譯類神經網路...")
+    model.compile(
+        optimizer=keras.optimizers.Nadam(),
+        loss=keras.losses.MSE)
+    return model
+
+
 if os.path.isfile('./Main_Reversi.h5'):
     print("讀取已保存的學習網路模型...")
-    Tmodel = keras.models.load_model('Main_Reversi.h5')
+    model = keras.models.load_model('Main_Reversi.h5')
 else:
     print('讀取錯誤!!重新建立學習網路模型...')
-    Tmodel = mt.create_model()
+    model = create_model()
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+Mweights = model.get_weights()
+Tweights = Mweights
 
-def predict_opt(chessboard):
+def predict_opt(chessboard,Main = True):
+    global model,Mweights,Tweights
     chessboard = chessboard.reshape(1, 64)
-    output = Tmodel.predict(chessboard).reshape(8, 8)
+    if Main:
+        model.set_weights(Mweights)
+    else:
+        model.set_weights(Tweights)
+    try:
+        output = model.predict(chessboard).reshape(8, 8)
+    except:
+        tf.keras.backend.set_session(session)
+        output = model.predict(chessboard).reshape(8, 8)
     return output
+
 
 loss = '未知'
 def train(input_data, expect_data, batch):
-    global loss
+    global loss,model,Tweights,Mweights
+    model.set_weights(Tweights)
     callback = [tf.keras.callbacks.EarlyStopping(
         monitor='loss', patience=100, min_delta=0.000001)]
     print(f'目前誤差: {loss}')
     print("開始訓練...")
-    Tmodel.fit(input_data, expect_data, epochs=10000,
+    model.fit(input_data, expect_data, epochs=1000,
                     batch_size=batch, verbose=0, callbacks=callback)
-    loss = Tmodel.evaluate(x=input_data, y=expect_data,
+    loss = model.evaluate(x=input_data, y=expect_data,
                           batch_size=batch, verbose=0, use_multiprocessing=True)
-    print(f'訓練後誤差: {loss}')
+    # print(f'訓練後誤差: {loss}')
+    Tweights = model.get_weights()
     if not os.path.isfile('./Main_Reversi.h5'):
-        Tmodel.save('Main_Reversi.h5')
+        model.save('Main_Reversi.h5')
     
 
 
-def update_weights(input_data, expect_data, batch):
-    global Tmodel
+def update_weights(input_data, expect_data, batch,Loss = True):
+    global model,Mweights,Tweights
     import Main_TrainingReversi as mt
-    print("更新網路權重為主要網路....")
-    Tmodel = keras.models.load_model('Main_Reversi.h5')
-    print("更新完成!!")
-
-
-def weights():
-    global Tmodel
-    return Tmodel.get_weights()
+    if Loss:    #輸主網路
+        print("更新網路權重為主要網路....")
+        model.set_weights(Mweights)
+        Tweights = Mweights
+        print("更新完成!!")
+    else:
+        model.set_weights(Tweights)
+        Mweights = Tweights
+        print("儲存...")
+        model.save('Main_Reversi.h5')
+        print("儲存完成!!")
